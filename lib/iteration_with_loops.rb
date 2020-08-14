49a635e65796205b751935c575a5b66368ab92e4
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-14 04:49:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   min_src = 100
   while row_index < src.count do
+    
     inner_index = 0
     while inner_index < src[row_index].count do
       if min_src > src[row_index][inner_index]
@@ -12,6 +13,7 @@ def find_min_in_nested_arrays(src)
       end
       inner_index += 1
     end
+    
   row_index += 1
   new_array << min_src
 end
